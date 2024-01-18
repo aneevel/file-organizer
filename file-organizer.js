9b9354b48dded40e7a495e5501739d30ac8a7385
Alec Neevel <aneevel15@gmail.com>
2024-01-18 03:05:57 -0600

    fail on non-existent input directory

--- file-organizer.js
+++ file-organizer.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 const path = require('path');
 
+const basePath = 'C:\\';
+
 if (process.argv.length !== 4)
 {
     console.error("Invalid input: file-organizer must be invoked as `node file-organizer.js {inputDirectory} {outputDirectory}");
     process.exit(1);
 }
 
-process.argv.forEach((value, index) => {
-    console.log(`${index}: ${value}`);
-});
+const inputDir = path.resolve(basePath, process.argv[2]);
+const outputDir = path.resolve(basePath, process.argv[3]);
 
-const inputDir = process.argv[2];
+// Input directory must exist, or no processing can occur
+if (!fs.existsSync(inputDir))
+{
+    console.error(`${inputDir} does not exist or is not an accessible directory. Unable to process files.`)
+    process.exit(1);
+}
+
+console.log(`Placing files from ${inputDir} into ${outputDir}`);
+
+// A non existent output directory should be created
