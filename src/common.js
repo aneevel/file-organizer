f2467fbbe6686a6c407ad6bc6e8bf6ece89cdefe
aneevel <aneevel15@gmail.com>
2024-06-09 00:35:32 -0500

    create common settings file

---
+++ src/common.js
@@ -0,0 +1,31 @@
+class Common {
+}
+
+Common.WINDOW_SIZE = {
+    width: 800,
+    height: 600,
+};
+
+Common.WINDOW_SIZE_SETTINGS = {
+    width: 800,
+    height: 600,
+};
+
+Common.MENU = {
+    about: 'About File Organizer',
+    hide: 'Hide Application',
+    hideOther: 'Hide Others',
+    showAll: 'Show All',
+    pref: 'Preference',
+    quit: 'Quit',
+    edit: 'Edit',
+    toggleFullScreen: 'Toggle Full Screen',
+    min: 'Minimize',
+    close: 'Close',
+    help: 'Help',
+    repo: 'GitHub Repository',
+    feedback: 'Report Issue',
+    checkRelease: 'Check for New Release',
+};
+
+module.exports = Common;
